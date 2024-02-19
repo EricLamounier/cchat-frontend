a6d3af878b3d9abe60b24eaf4a453bb548e3c525
Eric Lamounier <68248491+EricLamounier@users.noreply.github.com>
2024-02-19 09:22:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ const logIn = () => {
 
     
     // WebSocket
-    websocket = new WebSocket('wss://0.tcp.sa.ngrok.io:10054')
+    websocket = new WebSocket('wss://0.tcp.sa.ngrok.io:13353')
     websocket.onmessage = processMessage
 }
 
