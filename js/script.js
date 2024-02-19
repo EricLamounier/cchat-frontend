3ecf0404f6d02c3338120284eed5a7c32ea9c1a3
Eric Lamounier <68248491+EricLamounier@users.noreply.github.com>
2024-02-19 08:07:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ const logIn = () => {
 
     
     // WebSocket
-    websocket = new WebSocket('ws://8b22-179-189-86-218.ngrok-free.app')
+    websocket = new WebSocket('ws://8b22-179-189-86-218.ngrok-free.app:8080')
     websocket.onmessage = processMessage
 }
 
